5e1bf4f73c17d9ea3fd340234a3196cae6d12dcf
front-end-geek <lovepreetbrands@gmail.com>
2025-02-27 16:48:46 +0300

    route setup

--- prodev-mobile-app-0x04/app/Signin.tsx
+++ prodev-mobile-app-0x04/app/Signin.tsx
@@ -1,15 +1,15 @@
-import { Text, View, TextInput, TouchableOpacity, Image} from "react-native";
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
 import { styles } from "@/styles";
-import { SafeAreaProvider,SafeAreaView } from "react-native-safe-area-context";
-import {FontAwesome, Ionicons} from "@expo/vector-icons"
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View style={styles.navGroup}>
-        <Ionicons name="arrow-back" size={25} />
-        <Image source={require('@/assets/images/Logo.png')} />
+          <Ionicons name="arrow-back" size={25} />
+          <Image source={require('@/assets/images/logo.png')} />
         </View>
         <Text style={styles.largeText}>Sign in to your</Text>
         <Text style={styles.largeText}>Account</Text>
@@ -25,7 +25,7 @@ export default function Index() {
           <View style={{ marginTop: 20 }}>
             <Text style={styles.placeholderText}>Password</Text>
             <View style={styles.passwordGroup}>
-              <TextInput style={{ flex: 1 }} />
+              <TextInput style={{ flex: 1 }} secureTextEntry={true} />
               <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
             </View>
           </View>
@@ -37,47 +37,23 @@ export default function Index() {
         </TouchableOpacity>
 
         <View style={styles.dividerGroup}>
-          <View
-            style={styles.divider}
-          ></View>
-          <Text
-            style={styles.dividerText}
-          >
-            OR
-          </Text>
-          <View
-            style={styles.divider}
-          ></View>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
         </View>
 
         <View style={styles.socialMediaButtonGroup}>
           <TouchableOpacity style={styles.socialMediaButton}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-              }}
-            >
-              <Image source={require("@/assets/images/google.png")} />
-              <Text style={styles.socialMediaButtonText}>
-                Continue with Google
-              </Text>
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+              <Image source={require('@/assets/images/google.png')} />
+              <Text style={styles.socialMediaButtonText}>Continue with Google</Text>
             </View>
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.socialMediaButton}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-              }}
-            >
-              <Image source={require("@/assets/images/facebook.png")} />
-              <Text style={styles.socialMediaButtonText}>
-                Continue with Facebook
-              </Text>
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+              <Image source={require('@/assets/images/facebook.png')} />
+              <Text style={styles.socialMediaButtonText}>Continue with Facebook</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -86,7 +62,6 @@ export default function Index() {
           <Text style={styles.subText}>Don't have an account?</Text>
           <Text style={styles.subTextJoin}>Join now</Text>
         </View>
-
       </SafeAreaView>
     </SafeAreaProvider>
   );
